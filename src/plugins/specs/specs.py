ad32e556b0405bff569b3c4d9068bf497da80f73
Raphael 'kena' Poss <knz@thaumogen.net>
2014-06-15 20:29:53 +0200

    Improve the SCEP support, including displaying fingerprints.

--- src/plugins/specs/specs.py
+++ src/plugins/specs/specs.py
@@ -4,6 +4,7 @@ from git import Git, Repo, InvalidGitRepositoryError
 import os
 from datetime import datetime
 from time import mktime, altzone
+from sc import fp, fs
 
 import locale
 locale.setlocale(locale.LC_ALL, 'C')
@@ -20,9 +21,24 @@ def specs(content):
        or content.metadata.get('scep', None) is None:
         return
 
+    # compute fingerprint
+    fw = fs.fs_wrap(content.source_path)
+    cv = fp.compute_visitor()
+    fw.visit(cv)
+    content.fp = cv.fingerprint().compact()
+
     scep = int(content.metadata.get('scep'))
     content.template = 'spec'
     content.slug = 'scep%04d' % scep
+    content.source_file = content.source_path.split('/')[-1]
+    if scep > 0:
+        content.status = 'hidden'
+        content.title = content.slug.upper() + ' - ' + content.metadata.get('title')
+    else:
+        content.title = 'SCEPs'
+        content.status = 'published'
+    content.scep_status = content.metadata.get('status')
+    content.scep_title = content.metadata.get('title')
 
     path = content.source_path
     status, stdout, stderr = git.execute(['git', 'ls-files', path, '--error-unmatch'],
@@ -38,8 +54,6 @@ def specs(content):
         content.gitrev = commits[-1].id
 
     content.updated = content.updated.strftime('%F %T %z (%a, %d %B %Y)')
-    content.status = 'hidden'
-    content.scep_status = content.metadata.get('status')
 
 def register():
     signals.content_object_init.connect(specs)
